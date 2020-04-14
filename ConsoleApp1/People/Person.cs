80049b89f3581b892b2c84d73f12089187f0aead
Daryl Beebe <darylbeebe@gmail.com>
2020-04-14 12:59:51 -0700

    rev A

---
+++ ConsoleApp1/People/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.People
+{
+    public class Person
+    {
+        public string personName;
+        public int personAge;
+        public bool personHasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My name is " + personName + " and my age is " + personAge);
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using ConsoleApp1.People;
 
 namespace ConsoleApp1
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person();
+            person.personName = "John";
+            person.personAge = 28;
+            person.personHasPet = true;
+
+            person.Greeting();
+
+
+            /*    Console.WriteLine("Hello World!");
+
+                const string myName = "Daryl";
+                string nameUp = myName.ToUpper();
+                string tempName;
+
+                int numberA = 61;
+
+
+                Console.WriteLine(myName + nameUp);
+                Console.WriteLine(numberA);
+                Console.WriteLine(nameUp);
+
+                */
         }
     }
 }
